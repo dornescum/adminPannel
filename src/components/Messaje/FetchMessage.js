9fcb46b0da04f527273d086302e4fc8a2555ee65
Mihai Dornescu <dornescu@gmail.com>
2021-10-24 22:20:29 +0300

    temporary setup

--- src/components/Messaje/FetchMessage.js
+++ src/components/Messaje/FetchMessage.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import SingleMessage from "./SingleMessage";
+// const data = ['test1', 'test2', 'data3']
 
 const FetchMessage = () => {
+	const [message, setMessage] = useState([]);
+	const fetchMess = async () => {
+		const res = await fetch('https://jsonplaceholder.typicode.com/posts/');
+		const data = await res.json();
+		setMessage(data)
+	};
+	useEffect(() => {
+		fetchMess();
+	}, []);
+	// let i =0;
+
 	return (
 		<div>
-			message
+			{message.map((item) => {
+				// i++;
+				// if(i >= 11){
+				// 	return ;
+				// }
+				return <div key={item.id}>
+					<SingleMessage  {...item}/>
+				</div>;
+			})}
+
 		</div>
 	);
 };
--- src/components/Messaje/SingleMessage.js
+++ src/components/Messaje/SingleMessage.js
@@ -3,25 +3,27 @@ import styled from "styled-components";
 
 
 const SingleMessage = ({title, body}) => {
-	const [showMessage, setShowMessage] = useState(true)
-	const handleMessage =()=>{
-		setShowMessage(false)
-	}
-	return (
-		<div>
-			<button  onClick={handleMessage}>X</button>
-			{showMessage && <Single>
-				<h1>{title}</h1>
-				<p>{body}</p>
-			</Single>}
-			{/*<Single>*/}
-			{/*	/!*<button onClick={setShowMessage(!showMessage)}>X</button>*!/*/}
-			{/*	<h1>{title}</h1>*/}
-			{/*	<p>{body}</p>*/}
-			{/*</Single>*/}
-			{!showMessage && <p>No message</p>}
+	const [showMessage, setShowMessage] = useState(true);
+	const handleMessage = () => {
+		setShowMessage(!showMessage);
+	};
+	return (<MainSingle data-testid='main-single' className={showMessage ? 'block': 'flex'} >
+			<Btn onClick={handleMessage}>X</Btn>
+			<WrapperSingle>
+				{showMessage && <Single>
+					<h1>{title}</h1>
+					<p>{body}</p>
+				</Single>}
 
-		</div>
+				{/*<Single>*/}
+				{/*	/!*<button onClick={setShowMessage(!showMessage)}>X</button>*!/*/}
+				{/*	<h1>{title}</h1>*/}
+				{/*	<p>{body}</p>*/}
+				{/*</Single>*/}
+				{!showMessage && <h5 style={{padding: '0 1rem 0 0'}}>Hidden message</h5>}
+			</WrapperSingle>
+		</MainSingle>
+
 
 	);
 };
@@ -29,17 +31,38 @@ const SingleMessage = ({title, body}) => {
 export default SingleMessage;
 
 const Single = styled.div`
-//border: 1px solid var(--dark-color);
+  //border: 1px solid var(--dark-color);
   padding: 1rem .5rem;
-  margin: 1rem 0;
+  margin: 1rem 1rem 1rem 0;
   background: var(--light-color);
   border-radius: var(--radius);
+  width: 100%;
+
   h1 {
-	padding: .5rem 0 .5rem 2rem;
-	margin: .5rem 0 .5rem 0;
+    padding: .5rem 0 .5rem 2rem;
+    margin: .5rem 0 .5rem 0;
   }
+
   p {
     padding: .5rem 0 .5rem 2rem;
     margin: .5rem 0 .5rem 0;
   }
-`
+`;
+const WrapperSingle = styled.div`
+  display: flex;
+  justify-content: space-around;
+  position: relative;
+`;
+const Btn = styled.div`
+  background: red;
+  height: 1rem;
+  width: 1rem;
+  position: relative;
+  top: 2rem;
+  left: 1rem;
+  z-index: 1;
+`;
+const MainSingle = styled.div`
+  //display: ${({flex})=> flex || 'block'}
+  //justify-content: space-between;
+`;
