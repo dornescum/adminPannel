7257ecf17745a52411741a2286bc96e1add2489c
Mihai Dornescu <dornescu@gmail.com>
2021-10-25 21:17:04 +0300

    pagination not finished

--- src/components/Messaje/FetchMessage.js
+++ src/components/Messaje/FetchMessage.js
@@ -2,25 +2,31 @@ import React, {useState, useEffect} from 'react';
 import SingleMessage from "./SingleMessage";
 // const data = ['test1', 'test2', 'data3']
 
-const FetchMessage = () => {
+const FetchMessage = ({messages}) => {
 	const [message, setMessage] = useState([]);
-	const fetchMess = async () => {
-		const res = await fetch('https://jsonplaceholder.typicode.com/posts/');
-		const data = await res.json();
-		setMessage(data)
-	};
+	const [loading, setLoading]=useState(false);
+	const [currentPage, setCurrentPage]=useState(1);
+	const [messagesPerPage, setMessagesPerPage]= useState(10);
+
 	useEffect(() => {
+		const fetchMess = async () => {
+			setLoading(true);
+			const res = await fetch('https://jsonplaceholder.typicode.com/posts/');
+			const data = await res.json();
+			setMessage(data);
+			setLoading(false)
+		};
 		fetchMess();
 	}, []);
-	// let i =0;
+
+	const indexOfLastMessage = currentPage * messagesPerPage;
+	const indexOfFirstMessage = indexOfLastMessage - messagesPerPage;
+	const currentMessages = message.slice(indexOfFirstMessage, indexOfLastMessage);
+
 
 	return (
 		<div>
-			{message.map((item) => {
-				// i++;
-				// if(i >= 11){
-				// 	return ;
-				// }
+			{currentMessages.map((item) => {
 				return <div key={item.id}>
 					<SingleMessage  {...item}/>
 				</div>;
--- src/components/Messaje/Mesaje.js
+++ src/components/Messaje/Mesaje.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container} from "../UIElements/Container";
 import {Flex} from "../UIElements/Flex";
 import {Card} from "../UIElements/Card";
@@ -9,6 +9,22 @@ import FetchMessage from "./FetchMessage";
 import './mesaje.css'
 
 const Mesaje = () => {
+	// console.log(messages);
+	// const [users, setUsers] = useState(messages.slice(0, 200));
+	// const [pageNumber, setPageNumber] = useState(0);
+	//
+	// const userPerPage = 10;
+	// const pagesVisited = pageNumber * userPerPage;
+	// const displayUsers = users.slice(pagesVisited, pagesVisited + userPerPage).map((user)=>{
+	// 	return <FetchMessage
+	// 		first_name={user.first_name} last_name={user.last_name}
+	// 		date={user.date} message={user.text} key={user.id} id={user.id}/>
+	// })
+	// const pageCount =Math.ceil(users.length /userPerPage );
+	// const changePage =({selected})=>{
+	// 	setPageNumber(selected)
+	// }
+
 	return (
 		<Container>
 			<Article id='mesaje' data-testid='mesaje-fetch' layout='column' >
@@ -30,6 +46,7 @@ const Mesaje = () => {
 				</ol>
 			</Article>
 			<FetchMessage />
+
 		</Container>
 	);
 };
@@ -37,32 +54,32 @@ const Mesaje = () => {
 export default Mesaje;
 
 
-
-<div className="col-lg-12">
-	<div className="card card-default" id="cardStatisticiGenerale">
-		<div className="card-wrapper">
-			<div className="card-body">
-				<p className="blockquote mb-4">Aici puteti regasi mesajele primite prin intermediul
-					formularului de contact din pagina dvs. de prezentare/profil.</p>
-				<h4 className="pl-2">Cum functioneaza?</h4>
-				<ol className="my-4 fs-18">
-					<li className="py-2">
-						Clientul intra pe pagina dvs. de prezentare, completeaza formularul de contact apoi
-						apasa butonul "<i>Trimite mesaj</i>".
-					</li>
-					<li className="py-2">
-						Noi validam mesajul (sa nu fie SPAM, sa nu contina injurii etc.), apoi il trimitem
-						mai departe catre dvs.
-					</li>
-					<li className="py-2">
-						Primiti un email cu toate detaliile si veti avea posibilitatea sa raspundeti direct
-						clientului, prin reply la email.
-					</li>
-
-				</ol>
-			</div>
-		</div>
-	</div>
-
-
-</div>
+//
+// <div className="col-lg-12">
+// 	<div className="card card-default" id="cardStatisticiGenerale">
+// 		<div className="card-wrapper">
+// 			<div className="card-body">
+// 				<p className="blockquote mb-4">Aici puteti regasi mesajele primite prin intermediul
+// 					formularului de contact din pagina dvs. de prezentare/profil.</p>
+// 				<h4 className="pl-2">Cum functioneaza?</h4>
+// 				<ol className="my-4 fs-18">
+// 					<li className="py-2">
+// 						Clientul intra pe pagina dvs. de prezentare, completeaza formularul de contact apoi
+// 						apasa butonul "<i>Trimite mesaj</i>".
+// 					</li>
+// 					<li className="py-2">
+// 						Noi validam mesajul (sa nu fie SPAM, sa nu contina injurii etc.), apoi il trimitem
+// 						mai departe catre dvs.
+// 					</li>
+// 					<li className="py-2">
+// 						Primiti un email cu toate detaliile si veti avea posibilitatea sa raspundeti direct
+// 						clientului, prin reply la email.
+// 					</li>
+//
+// 				</ol>
+// 			</div>
+// 		</div>
+// 	</div>
+//
+//
+// </div>
